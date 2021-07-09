c2d4b9e8094260a29ccb273b4f6cb5167ce9c002
rauleduardogutierrez <71554684+rauleduardogutierrez@users.noreply.github.com>
2021-07-09 18:04:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
- function calculate()
-            {
-                p = document.getElementById("p").value;
-                n = document.getElementById("n").value;
-                r = document.getElementById("r").value;
-                result = document.getElementById("result");
-                
-                result.innerHTML = "The interest is " + (p*n*r/100);
-            }
+function compute()
+{
+    var amountnumber = document.getElementById("amountnumber").value;
+
+    if(amountnumber == "" || amountnumber <= 0)
+    {
+        alert("Enter a positive number of amount");
+        document.getElementById("amountnumber").focus();
+        return;
+    }
+
+    var years = document.getElementById("years").value;
+
+  
+    var num = document.getElementById("num").value;
+    var years = document.getElementById("years").value;
+    var interest = amountnumber * years * num / 100;
+
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML =  "<br>If you deposit " + "<span class='highlight'>" + amountnumber + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + num + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
+}
+
+function SliderValue()
+{
+    var slider = document.getElementById("num");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function()
+    {
+        output.innerHTML = this.value;
+    }
+}
